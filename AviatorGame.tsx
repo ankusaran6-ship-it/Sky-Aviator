08cc3685541b92998787333062b16d32882458e1
ankusaran6-ship-it <ankusaran6@gmail.com>
2026-02-07 22:18:10 +0530

    Create AviatorGame.tsx

---
+++ AviatorGame.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const AviatorGame = ({multiplier}) => <div className="p-10 bg-gray-900 rounded-3xl text-center text-4xl font-black">{multiplier}x</div>;
+export default AviatorGame;
